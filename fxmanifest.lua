0359636d588dd8ca5cd6b68f2aec40dd0fae06be
JellyPh1sh <sacha.sorgiati@gmail.com>
2023-07-04 15:54:02 +0200

    :rocket: | Deploy resource structure.

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version "cerulean"
+game "gta5"
+
+version "1.0"
+description "All basic utilities."
+author "JellyPh1sh"
+
+client_scripts {
+    "client/*.lua"
+}
+
+server_scripts {
+    "server/*.lua"
+}
